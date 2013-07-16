d80af8226fbe028ab11659d4e27c106b03f6c55b
Matthew Jiang <matthewljiang@gmail.com>
2013-07-16 18:41:36 -0400

    Update migrations for all models

--- Curiax_app/db/migrate/20130713184603_create_companies.rb
+++ Curiax_app/db/migrate/20130713184603_create_companies.rb
@@ -1,8 +1,9 @@
 class CreateCompanies < ActiveRecord::Migration
   def change
     create_table :companies do |t|
-
-      t.timestamps
+    	t.string :comp_name
+    	t.text :comp_description
+    	t.timestamps
     end
   end
 end
--- Curiax_app/db/migrate/20130713184620_create_seminars.rb
+++ Curiax_app/db/migrate/20130713184620_create_seminars.rb
@@ -1,7 +1,8 @@
 class CreateSeminars < ActiveRecord::Migration
   def change
     create_table :seminars do |t|
-
+    	t.string :sem_name
+    	t.text :sem_description
       t.timestamps
     end
   end
